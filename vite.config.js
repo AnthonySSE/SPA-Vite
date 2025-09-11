567970c6330d98894728fc0adabbde0f582919a2
AnthonySSE <anthonyasse17@gmail.com>
2025-09-11 17:28:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/vita-vanilla-js-app/" : "/",
   root: ".",
   build: {
     outDir: "dist",
+    assetsDir: "assets",
   },
   server: {
     port: 3000,
